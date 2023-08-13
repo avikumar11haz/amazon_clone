de1e69e4d85117132d3fcb0c158ddf2b7b486d55
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-08-13 16:13:15 +0530

    add to db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const authRouter = require('./routes/auth')
 
 const PORT = 3000;
 
 const app = express();
+const DB = "mongodb+srv://haz56:sendmesomething@cluster0.b7usahc.mongodb.net/?retryWrites=true&w=majority"
+
+app.use(express.json());
+app.use(authRouter);
+
+mongoose.connect(DB).then(() => {
+console.log("Connection Successful")}).catch((e) => {
+console.log(e);
+});
 
 app.listen(PORT, () => {
 console.log(`connected at port ${PORT} `);
