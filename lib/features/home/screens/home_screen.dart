0718641609d83689de33e5cc3c53c5f59da7cd4f
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-08-15 18:02:30 +0530

    deal of day widget

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:amazone_clone/constants/global_variables.dart';
 import 'package:amazone_clone/features/home/widgets/address_box.dart';
 import 'package:amazone_clone/features/home/widgets/carousel_image.dart';
+import 'package:amazone_clone/features/home/widgets/deal_of_day.dart';
 import 'package:amazone_clone/features/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
 
@@ -89,16 +90,19 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: const Column(
-        children: [
-          AddressBox(),
-          SizedBox(
-            height: 10,
-          ),
-          TopCategories(),
-          SizedBox(height: 10,),
-          CarouselImage(),
-        ],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            AddressBox(),
+            SizedBox(
+              height: 10,
+            ),
+            TopCategories(),
+            SizedBox(height: 10,),
+            CarouselImage(),
+            DealOfDay(),
+          ],
+        ),
       ),
     );
   }
