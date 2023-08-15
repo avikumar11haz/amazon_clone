5067e1f94ca23362e64f36095133bb67703ed895
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-08-15 15:04:14 +0530

    top buttons

---
+++ lib/features/account/widgets/top_buttons.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TopButtons extends StatefulWidget {
+  const TopButtons({Key? key}) : super(key: key);
+
+  @override
+  State<TopButtons> createState() => _TopButtonsState();
+}
+
+class _TopButtonsState extends State<TopButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+
+          ],
+        )
+      ],
+    );
+  }
+}
